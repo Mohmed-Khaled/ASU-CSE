df011581c7ef5d8c5abf4a552a563c9373d479e8
Mohamed_Khaled <mr_tlmk@hotmail.com>
2015-12-09 05:52:36 +0200

    DbC added to Dictionary.cpp

--- Assignment 2/Dictionary.cpp
+++ Assignment 2/Dictionary.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
+#include "dbc.h"
+#include <regex>
 
 using namespace std;
-
-
 
 class Dictionary 
 {
@@ -11,20 +11,94 @@ private:
 	string names[100];
 	string emails[100];
 	int size;
+	bool checkName(string name)
+	{
+
+		regex nameRegex("[A-Za-z][A-Za-z', ]*");
+		if (regex_match(name, nameRegex))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool checkEmail(string email)
+	{
+		if (email == "")
+		{
+			return false;
+		}
+		int atPos = -1;
+		int dotPos = -1;
+		for (int i = 0, len = email.length(); i < len;i++)
+		{
+			if (email[i] == '@')
+			{
+				atPos = i;
+			}
+			else if (email[i] == '.')
+			{
+				dotPos = i;
+			}
+		}
+		if (atPos == -1 || dotPos == -1)
+		{
+			return false;
+		}
+		if (atPos < 1 || dotPos < atPos + 2 || dotPos == email.length() - 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	bool nameExist(string name)
+	{
+		for (int i = 0; i<size; i++)
+		{
+			if (names[i] == name)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 public:
+	// default constructor
 	Dictionary()
 	{
 		size =0;
 	}
+	// commands
 	void Add(string name,string email)
 	{
+		int entrySize;
+		INVARIANT0(size < 100);
+		REQUIRE0((entrySize = getSize()) || 1);
+		REQUIRE0(checkName(name));
+		REQUIRE0(checkEmail(email));
+		//body start
 		names[size] = name;
 		emails[size] = email;
 		size++;
+		//body end
+		ENSURE0(nameExist(name));
+		ENSURE0(getEmail(name) == email);
+		ENSURE0(getSize() == entrySize + 1);
+		INVARIANT0(size < 100);
 	}
 
 	void Remove(string name)
 	{
+		int entrySize;
+		INVARIANT0(size < 100);
+		REQUIRE0((entrySize = getSize()) || 1);
+		REQUIRE0(checkName(name));
+		//body start
 		// a simple example on removing array of numbers
 		// if the array is  {1,5,3,4,2,6,7,8,9,10}
 		// and its size is 10 elements
@@ -48,12 +122,37 @@ public:
 			names[i] = names[i+1];
 			emails[i] = emails[i+1];
 		}
+		//body end
+		ENSURE0(!nameExist(name));
+		ENSURE0(getSize() == entrySize - 1);
+		INVARIANT0(size < 100);
 	}
 	void printentries()
 	{
-		for(int i =0;i<size;i++)
+		for(int i = 0;i < size;i++)
 		{
-			cout<<"Entry #"<<i+1<<":"<<endl<<names[i]<<": "<<emails[i]<<endl;
+			cout << "Entry #" << i+1 << ":" << endl << names[i] << ": " << emails[i] <<endl;
+		}
+	}
+	int getSize() 
+	{ 
+		return size; 
+	}
+	string getEmail(string name) 
+	{ 
+		int index = -1;
+
+		for (int i = 0; i<size; i++)
+		{
+			if (names[i] == name)
+			{
+				index = i;
+				break;
+			}
+		}
+		if (index != -1)
+		{
+			return emails[index];
 		}
 	}
 };
